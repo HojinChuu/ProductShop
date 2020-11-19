099ace972eaeea8711ab4ea182f53ed69f42564e
HojinChuu <cnghwls7@gmail.com>
2020-11-19 23:52:12 +0900

    build

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,22 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
+
 const uploadPath = path.join(__dirname, "/uploads");
 app.use("/uploads", express.static(uploadPath));
+
+const buildPath = path.join(__dirname, "/frontend/build");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(buildPath));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
@@ -43,5 +57,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
